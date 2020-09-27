fc4aad52f371b7e00fd21ad4875ed390685cf587
= <tranminhtri9090@gmail.com>
2020-09-28 00:10:31 +1000

    Adding Posts and nav-Options  into mySQL databases and render it dynamically using PHP

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php 
+    include('./includes/db.php');
     include('./includes/header.php');
 ?>
     <!-- Navigation -->
@@ -20,21 +21,34 @@
                 </h1>
 
                 <!-- First Blog Post -->
-                <h2>
-                    <a href="#">Blog Post Title</a>
-                </h2>
-                <p class="lead">
-                    by <a href="index.php">Start Bootstrap</a>
-                </p>
-                <p><span class="glyphicon glyphicon-time"></span> Posted on August 28, 2013 at 10:00 PM</p>
-                <hr>
-                <img class="img-responsive" src="http://placehold.it/900x300" alt="">
-                <hr>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, veritatis, tempora, necessitatibus inventore nisi quam quia repellat ut tempore laborum possimus eum dicta id animi corrupti debitis ipsum officiis rerum.</p>
-                <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+                <?php 
+                    $query = "SELECT * FROM posts";
+                    $postRead = mysqli_query($connection, $query);
 
-                <hr>
+                    while ($row = mysqli_fetch_assoc($postRead)) {
+                        $post_title = $row["post_title"];
+                        $post_author = $row["post_author"];
+                        $post_date = $row["post_date"];
+                        $post_img = $row["post_image"];
+                        $post_content = $row["post_content"];
 
+                        echo "
+                            <h2>
+                                <a href='#'>{$post_title}</a>
+                            </h2>
+                            <p class='lead'>
+                                by <a href='index.php'>{$post_author}</a>
+                            </p>
+                            <p><span class='glyphicon glyphicon-time'></span> Posted on {$post_date}</p>
+                            <hr>
+                            <img class='img-responsive' src='{$post_img}' alt=''>
+                            <hr>
+                            <p>{$post_content}</p>
+                            <a class='btn btn-primary' href='#'> Read More <span class='glyphicon glyphicon-chevron-right'></span></a>
+                            <hr> 
+                        ";
+                    }
+                ?>
             </div>
 
             <!-- Blog Sidebar Widgets Column -->
