939c9b52bebef3feeba664ef822a8888fafdc5df
= <tranminhtri9090@gmail.com>
2020-10-05 16:30:19 +1000

    Updating databases to use config vars from heroku

--- includes/db.php
+++ includes/db.php
@@ -1,6 +1,31 @@
 <?php 
     // Make a connection between PHP and MySQL database
-    $connection = mysqli_connect('localhost', 'root', 'root', 'cms', 3307);
+    $host = getenv('DB_HOST');
+    if (isset($host)) {
+        $host = 'localhost';
+    }
+
+    $username = getenv('DB_USERNAME');
+    if (isset($username)) {
+        $username = 'root';
+    }
+
+    $password = getenv('DB_PASSWORD');
+    if (isset($password)) {
+        $password = 'root';
+    }
+
+    $databaseName = getenv('DB_DATABASE');
+    if (isset($databaseName)) {
+        $databaseName = 'cms';
+    }
+
+    $port = getenv('PORT');
+    if (isset($port)) {
+        $port = 3307;
+    }
+
+    $connection = mysqli_connect($host, $username, $password, $databaseName, $port);
 
     if (!$connection) {
         echo "We are NOT connected";
