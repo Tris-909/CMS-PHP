3f503490c97b48fe689b190b1cb4c4bc1c034c3c
= <tranminhtri9090@gmail.com>
2020-10-05 17:24:25 +1000

    Fixing that bug again

--- includes/db.php
+++ includes/db.php
@@ -1,31 +1,31 @@
 <?php 
     // Make a connection between PHP and MySQL database
-    $host = getenv('DB_HOST');
-    // if (empty($host)) {
-    //     $host = 'localhost';
-    // }
+    // $host = getenv('DB_HOST');
+    // // if (empty($host)) {
+    // //     $host = 'localhost';
+    // // }
 
-    $username = getenv('DB_USERNAME');
-    // if (empty($username)) {
-    //     $username = 'root';
-    // }
+    // $username = getenv('DB_USERNAME');
+    // // if (empty($username)) {
+    // //     $username = 'root';
+    // // }
 
-    $password = getenv('DB_PASSWORD');
-    // if (empty($password)) {
-    //     $password = 'root';
-    // }
+    // $password = getenv('DB_PASSWORD');
+    // // if (empty($password)) {
+    // //     $password = 'root';
+    // // }
 
-    $databaseName = getenv('DB_DATABASE');
-    // if (empty($databaseName)) {
-    //     $databaseName = 'cms';
-    // }
+    // $databaseName = getenv('DB_DATABASE');
+    // // if (empty($databaseName)) {
+    // //     $databaseName = 'cms';
+    // // }
 
-    $port = getenv('PORT');
-    // if (empty($port)) {
-    //     $port = 3307;
-    // }
+    // $port = getenv('PORT');
+    // // if (empty($port)) {
+    // //     $port = 3307;
+    // // }
 
-    $connection = mysqli_connect($host, $username, $password, $databaseName, $port);
+    $connection = mysqli_connect('remotemysql.com', 'OTM2AB53Xq', '7pZPhm9mlw', 'OTM2AB53Xq', 3306);
 
     if (!$connection) {
         echo $host;
